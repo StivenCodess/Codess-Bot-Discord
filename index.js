7deae72044db2101f7f44d8cf850c57027e524aa
Tiven <vairtor@gmail.com>
2023-01-31 18:54:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
+const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember } = Partials;
+require("dotenv").config();
+const client = new Client({
+  intents: [Guilds, GuildMembers, GuildMessages],
+  partials: [User, Message, GuildMember, ThreadMember],
+});
+
+const { loadEvents } = require("./Handlers/eventHandlers");
+
+client.events = new Collection();
+
+loadEvents(client);
+
+client.login(process.env.DISCORD_TOKEN);
